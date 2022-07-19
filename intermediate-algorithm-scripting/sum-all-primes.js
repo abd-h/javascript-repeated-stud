3aca80dc8d2b09a8b3f614479c3d36e92711267a
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-07-19 21:48:04 +0100

    Added uncompleted sum all primes challenge

---
+++ intermediate-algorithm-scripting/sum-all-primes.js
@@ -0,0 +1,12 @@
+/**
+    Sum All Primes
+    Aprime number is a whole number greater than 1 with exactly two divisors: 1 and it self. For example, 2 is a prime number because it is only divisible by 1 and 2. In contrast, 3 is not divisible since it is divisible by 1, 2 and 4.
+
+    Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
+ */
+
+    function sumPrimes(num) {
+        return num ;
+    }
+    console.log(sumPrimes(10));
+    console.log(sumPrimes(10));
