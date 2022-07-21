c00cfb7f38623dc6500ae28124c7e4ace8472a76
ABDALLA HUSSEIN <A.HUSSEIN1934@gmail.com>
2022-07-21 21:37:52 +0100

    Solved Sum All Primes challenge

--- intermediate-algorithm-scripting/sum-all-primes.js
+++ intermediate-algorithm-scripting/sum-all-primes.js
@@ -5,13 +5,26 @@
     Rewrite sumPrimes so it returns the sum of all prime numbers that are less than or equal to num.
  */
 
-    function sumPrimes(num) {
-        let newNum = 3;
-        for(let i = 1; i < num; i++) {
-            if(num % i == 0) {
-                console.log(i);
+    function isPrime(n) {
+        for(let i = 2; i < n; i++) {                    
+            if(n % i === 0) {
+                return false;
             }
         }
-
+        return true;
     }
-    console.log(sumPrimes(10));
+    console.log(isPrime(9));
+
+    function sumPrimes(num) {
+        let newNum = 0;                                   
+        for(let i = 2; i <= num; i++) {
+            if(isPrime(i) && i <= num) {
+                newNum += i;
+            }
+        }
+        return newNum;
+       
+    }
+   console.log(sumPrimes(10)); // should return a number.
+   console.log(sumPrimes(10)); // should return 17.
+   console.log(sumPrimes(977)); // should return 73156.
